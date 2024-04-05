55f6548fff843a396dc581fcbcfb5897bcd97577
Balendra Kasera <123406819+BalendraKasera@users.noreply.github.com>
2024-04-05 16:15:19 +0000

    changes done

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountId=220231013;
+let accountEmail="balendra@gmail.com";
+var userName="balendra"
+console.table([accountId,accountEmail,userName]);
+// do not use var in future because of scope problem and functional problem
+let account;
+console.table([account]);
